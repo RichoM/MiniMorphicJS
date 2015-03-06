94eccdb18178e944b9386902d0b410c18dba006a
RichoM <richi.moran@gmail.com>
2015-03-06 14:23:38 -0300

    Second pass on the optimization of fountains.

--- js/demo/OptimizedFountain.js
+++ js/demo/OptimizedFountain.js
@@ -1,25 +1,15 @@
 
 var OptimizedFountain = (function () {
+
+	var MAX_PARTICLES = 4000; // How many particles we support
     
     OptimizedFountain.inherits(Morph);
     function OptimizedFountain() {
         Morph.call(this);
-				
-		var particles = [];
-		this.particles = function (val) { 
-			if (val !== undefined) {
-				particles = val;
-			}
-			return particles; 
-		};
-		
-		var started = false;
-		this.started = function (val) {
-			if (val !== undefined) {
-				started = val;
-			}
-			return started;
-		};
+
+		this.index = 0;
+		this.particles = new Array(MAX_PARTICLES * 4);
+		this.started = false;
 		
 		/*
 		Make it as big as the world. ACAACA Richo: I should have a better way of 
@@ -31,13 +21,18 @@ var OptimizedFountain = (function () {
     
 	OptimizedFountain.methods({
 		drawOn: function (canvas) {
-			var particles = this.particles();
-			for (var i = 0; i < particles.length; i++) {
-				var particle = particles[i];
-				canvas.withAlpha(particle.alpha, function () {
+			var bottom = this.bottom();
+			var particles = this.particles;
+			for (var i = 0; i < particles.length; i += 4) {
+				var x = particles[i];
+				if (x === undefined) continue;
+				var y = particles[i + 1];
+				if (y > bottom) continue;
+				
+				canvas.withAlpha(1, function () {
 					canvas.fillRectangle({
-						x: particle.x,
-						y: particle.y,
+						x: x,
+						y: y,
 						w: 5,
 						h: 5
 					}, "blue");
@@ -45,42 +40,52 @@ var OptimizedFountain = (function () {
 			}
 		},
 		start: function () {
-			this.started(true);
+			this.started = true;
 			var center = this.center();
-			var maxTime = 4000; // Max lifetime of each particle (in ms)
 			
 			this.on("step", function (now) {
 				// Update bounds just in case the windows was resized
 				this.bounds(World.current().bounds());
 				
-				var particles = this.particles();
-				
-				// Move all particles
-				var keep = []; // Which particles will survive this generation
-				for (var i = 0; i < particles.length; i++) {
-					var particle = particles[i];
-					particle.x += particle.speed.x;
-					particle.y += particle.speed.y;
+				var particles = this.particles;
+				for (var i = 0; i < particles.length; i += 4) {
+					var x = particles[i];
+					if (x === undefined) continue;
 					
-					particle.speed.y += 0.1; // Accelerate
+					var y = particles[i + 1];
+					var dx = particles[i + 2];
+					var dy = particles[i + 3];
 					
-					var progress = now - particle.startTime;
-					particle.alpha = 1 - (progress / maxTime);
+					// Move
+					x += dx;
+					y += dy;
 					
-					// Only keep young particles
-					if (progress <= maxTime) {
-						keep.push(particle);
-					}
+					// Accelerate
+					dy += 0.1;
+					
+					// Update
+					particles[i] = x;
+					particles[i + 1] = y;
+					/* dx is constant */
+					particles[i + 3] = dy;
 				}
 				
-				// Remove old particles
-				this.particles(keep);
-				particles = keep;
-				
 				// Create new particles
-				if (this.started()) {
-					for (var i = 0; i < 25; i++) {
-						this.createNewParticle(now, center, maxTime, particles);
+				if (this.started) {
+					var extent = this.extent();
+					var s = MAX_PARTICLES / 200; // How many particles we'll add
+					for (var i = 0; i < s * 4; i += 4) {
+						var x = center.x - 2.5;
+						var y = center.y - 2.5;
+						var dx = Math.random() * (extent.w / 200) - (extent.w / 400);
+						var dy = Math.random() * -1 - (extent.h / 150);
+						
+						var index = this.index * 4;
+						particles[index + i] = x;
+						particles[index + i + 1] = y;
+						particles[index + i + 2] = dx;
+						particles[index + i + 3] = dy;
+						this.index = (this.index + 1) % MAX_PARTICLES;
 					}
 				}
 			});
@@ -91,29 +96,19 @@ var OptimizedFountain = (function () {
 			});
 		},
 		stop: function () {
-			this.started(false);
+			this.started = false;
 			// Remove myself when no more particles left
 			this.on("step", function () {
-				if (this.particles().length === 0) {
+				var count = 0;
+				for (var i = 0; i < this.particles.length; i += 4) {
+					if (this.particles[i] !== undefined) {
+						count++;
+					}
+				}
+				if (count === 0) {
 					this.remove();
 				}
 			});
-		},
-		createNewParticle: function (startTime, center, maxTime, particles) {
-			var extent = this.extent();
-			
-			var particle = {
-				x: center.x - 2.5, 
-				y: center.y - 2.5,
-				startTime: startTime,
-				alpha: 1,
-				speed: {
-					x: Math.random() * (extent.w / 200) - (extent.w / 400),
-					y: Math.random() * -1 - (extent.h / 150)
-				},				
-			};
-
-			particles.push(particle);
 		}
 	});
 	
