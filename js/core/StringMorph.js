568227df34cedb5c2202bb2c5b0ba867e7661d46
Matias Teragni <matias.teragni@gmail.com>
2018-09-18 15:15:21 -0300

    Now the StringMorph changes its width when is provided with a new text

--- js/core/StringMorph.js
+++ js/core/StringMorph.js
@@ -1,25 +1,31 @@
 class StringMorph extends Morph {
 	constructor(text) {
 		super();
-		this._text = text;
-		this.color="#AAAAAA";
+		this.text = text;
+		this.color = "#AAAAAA";
 	}
 	get text() {
 		return this._text;
 	}
 	set text(v) {
 		this._text = v;
+		let canvas = document.createElement("canvas"); 
+		let ctx = canvas.getContext("2d");
+		ctx.font = "18px Arial";
+		ctx.textAlign = "left";
+		ctx.textBaseline = "top";
+		let metrics = ctx.measureText(this.text);
+		this.width=metrics.width;
 		this.changed();
 		return this._text;
 	}
 	drawOn(canvas) {
-		let m = canvas.drawText(
+		canvas.drawText(
 			this.position,
 			this.text,
 			this.color,
 			"18px Arial",
 			"left",
 			"top");
-		this.width=m.width; 
 	}
 }
