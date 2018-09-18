986773a3a666206d760112e79ac49cec9be2749b
Matias Teragni <matias.teragni@gmail.com>
2018-09-18 12:30:09 -0300

    Implemented BouncingBall as a class

--- js/demo/BouncingBall.js
+++ js/demo/BouncingBall.js
@@ -1,44 +1,45 @@
+class BouncingBall extends Ellipse {
+	constructor() {
+		super();
+		//TODO: use private fields when it is on the ECMA standard
+		this._speed = Math.random() * -2.5;
+		let extent = Math.random() * 150;
 
-var BouncingBall = (function () {
-    
-    BouncingBall.inherits(Ellipse);
-    function BouncingBall() {
-        Ellipse.call(this);
-		
-		var speed = Math.random() * -2.5;
-		this.speed = function () { return speed; };
-		
-		var world = World.current();
-		var extent = Math.random() * 150;
-		this.color("red");
-		this.extent({ w: extent, h: extent });
-		this.alpha(Math.random());
-		this.center({
-			x: Math.random() * world.width(),
-			y: Math.random() * (world.height() * 0.5)
-		});
-		
+		this.color = "red";
+		this.extent = {
+			w : extent,
+			h : extent
+		};
+		this.alpha = Math.random();
+		let world = World.current();
+		this.center = {
+			x : Math.random() * world.width(),
+			y : Math.random() * (world.height() * 0.5)
+		};
+
 		this.on("step", function () {
 			// Fall
-			var center = this.center();		
-			this.center({ 
-				x: center.x,
-				y: center.y + speed
-			});
-			
-			speed += 0.1; // Accelerate
-			
+			let center = this.center;
+			this.center = {
+				x : center.x,
+				y : center.y + speed
+			};
+
+			this._speed += 0.1; // Accelerate
+
 			// Bounce
-			if (speed > 0 // Only if it's falling
-					&& this.bottom() >= world.bottom()) {
+			if (this.speed > 0 // Only if it's falling
+				 && this.bottom >= world.bottom) {
 				this.bottom(world.bottom());
-				speed *= -0.75;
-				if (Math.abs(speed) <= 1) {
-					speed = 0;
+				this._speed *= -0.75;
+				if (Math.abs(this.speed) <= 1) {
+					this._speed = 0;
 				}
 			}
 		});
-    }
-    
-    return BouncingBall;
-})();
+	}
+
+	get speed() {
+		return this._speed;
+	}
+}
