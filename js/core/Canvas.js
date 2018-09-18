8a8ca4778dd235aaddc116ea26f7d47713988d2f
Matias Teragni <matias.teragni@gmail.com>
2018-09-18 10:53:47 -0300

    Implemented Canvas as a class

--- js/core/Canvas.js
+++ js/core/Canvas.js
@@ -1,45 +1,54 @@
+class Canvas {
+	constructor(html) {
+		//TODO: use private fields when it is on the ECMA standard
+		this._html = html;
+		this._ctx = html.getContext("2d");
+	}
+	get ctx() {
+		return this._ctx;
+	}
+	get html() {
+		return this._html;
+	}
 
-var Canvas = (function () {
-    
-    function Canvas(html) {
-        var ctx = html.getContext("2d");        
-        
-        this.html = function () { return html; };
-        this.ctx = function () { return ctx; };
-        
-        this.fillRectangle = function (rect, fillStyle) {
-            ctx.fillStyle = fillStyle;
-            ctx.fillRect(rect.x, rect.y, rect.w, rect.h);
-        };
-		this.fillArc = function (x, y, radius, startAngle, endAngle, anticlockwise, fillStyle) {
-			ctx.beginPath();
-			ctx.arc(x, y, radius, startAngle, endAngle, anticlockwise);
-			ctx.closePath();
-			ctx.fillStyle = fillStyle;
-			ctx.fill();
-		};
-        this.drawImage = function (rect, form) {
-            ctx.drawImage(form.img(), rect.x, rect.y, rect.w, rect.h);
-        };
-        this.clearRectangle = function (rect) {
-            ctx.clearRect(rect.x, rect.y, rect.w, rect.h);
-        };
-        this.drawText = function (position, text, fillStyle, font, textAlign, textBaseline) {
-            ctx.save();
-            if (fillStyle) { ctx.fillStyle = fillStyle; }
-            if (font) { ctx.font = font; }
-            if (textAlign) { ctx.textAlign = textAlign; }
-            if (textBaseline) { ctx.textBaseline = textBaseline; }
-            ctx.fillText(text, position.x, position.y);
-            ctx.restore();
-        };
-        this.withAlpha = function (alpha, callback, that) {
-            var oldAlpha = ctx.globalAlpha;
-            ctx.globalAlpha = alpha;
-            callback.apply(that);
-            ctx.globalAlpha = oldAlpha;
-        };
-    }
-    
-    return Canvas;
-})();
+	fillRectangle(rect, fillStyle) {
+		this.ctx.fillStyle = fillStyle;
+		this.ctx.fillRect(rect.x, rect.y, rect.w, rect.h);
+	}
+	fillArc(x, y, radius, startAngle, endAngle, anticlockwise, fillStyle) {
+		this.ctx.beginPath();
+		this.ctx.arc(x, y, radius, startAngle, endAngle, anticlockwise);
+		this.ctx.closePath();
+		this.ctx.fillStyle = fillStyle;
+		this.ctx.fill();
+	}
+	drawImage(rect, form) {
+		this.ctx.drawImage(form.img(), rect.x, rect.y, rect.w, rect.h);
+	}
+	clearRectangle(rect) {
+		this.ctx.clearRect(rect.x, rect.y, rect.w, rect.h);
+	}
+	drawText(position, text, fillStyle, font, textAlign, textBaseline) {
+		this.ctx.save();
+		if (fillStyle) {
+			this.ctx.fillStyle = fillStyle;
+		}
+		if (font) {
+			this.ctx.font = font;
+		}
+		if (textAlign) {
+			this.ctx.textAlign = textAlign;
+		}
+		if (textBaseline) {
+			this.ctx.textBaseline = textBaseline;
+		}
+		this.ctx.fillText(text, position.x, position.y);
+		this.ctx.restore();
+	}
+	withAlpha(alpha, callback, that) {
+		var oldAlpha = this.ctx.globalAlpha;
+		this.ctx.globalAlpha = alpha;
+		callback.apply(that);
+		this.ctx.globalAlpha = oldAlpha;
+	}
+}
