f7da5174910fd462967385150ace0875d2b61ddf
Matias Teragni <matias.teragni@gmail.com>
2018-09-18 12:52:27 -0300

    Fixed Bouncing balls

--- js/demo/BouncingBall.js
+++ js/demo/BouncingBall.js
@@ -13,8 +13,8 @@ class BouncingBall extends Ellipse {
 		this.alpha = Math.random();
 		let world = World.current();
 		this.center = {
-			x : Math.random() * world.width(),
-			y : Math.random() * (world.height() * 0.5)
+			x : Math.random() * world.width,
+			y : Math.random() * (world.height * 0.5)
 		};
 
 		this.on("step", function () {
@@ -22,7 +22,7 @@ class BouncingBall extends Ellipse {
 			let center = this.center;
 			this.center = {
 				x : center.x,
-				y : center.y + speed
+				y : center.y + this._speed
 			};
 
 			this._speed += 0.1; // Accelerate
@@ -30,7 +30,7 @@ class BouncingBall extends Ellipse {
 			// Bounce
 			if (this.speed > 0 // Only if it's falling
 				 && this.bottom >= world.bottom) {
-				this.bottom(world.bottom());
+				this.bottom=world.bottom;
 				this._speed *= -0.75;
 				if (Math.abs(this.speed) <= 1) {
 					this._speed = 0;
