5cf39a8113c677c9b55ab9554aad6332d1b405f7
Matias Teragni <matias.teragni@gmail.com>
2018-09-18 11:15:15 -0300

    Implemented EventHandler as a class

--- js/core/EventHandler.js
+++ js/core/EventHandler.js
@@ -1,87 +1,81 @@
+class EventHandler {
+	constructor() {
+		//TODO: use private fields when it is on the ECMA standard
+		this._eventListeners = {};
+		this._handlesMouseMove = false;
+		this._handlesKeyboard = false;
+		this._handlesStepping = false;
+	}
+	get wantsToHandleMouseMove() {
+		return this._handlesMouseMove;
+	}
+	get wantsToHandleKeyboard() {
+		return this._handlesKeyboard;
+	}
+	get wantsToHandleStepping() {
+		return this._handlesStepping;
+	}
+	listenersFor(evtType) {
+		return this._eventListeners[evtType] || [];
+	}
+	registerListener(evtType, listener, that) {
+		let listeners = this._eventListeners[evtType];
+		if (listeners === undefined) {
+			listeners = [];
+			this._eventListeners[evtType] = listeners;
+		}
+		if (evtType === "mouseEnter" ||
+			evtType === "mouseMove" ||
+			evtType === "mouseLeave") {
+			this.handlesMouseMove = true;
+		} else if (evtType === "keyDown" ||
+			evtType === "keyUp") {
+			this.handlesKeyboard = true;
+		} else if (evtType === "step") {
+			this.handlesStepping = true;
+		}
+		listeners.push({
+			that : that,
+			callback : listener
+		});
+	}
+	forgetListenersFor(evtType) {
+		this._eventListeners[evtType] = undefined;
+	}
 
-var EventHandler = (function () {
-    
-    function EventHandler() {        
-        var eventListeners = {};
-        var handlesMouseMove = false;
-        var handlesKeyboard = false;
-        var handlesStepping = false;
-        
-        this.listenersFor = function (evtType) {
-            return eventListeners[evtType] || [];
-        };
-        this.registerListener = function (evtType, listener, that) {
-            var listeners = eventListeners[evtType];
-            if (listeners === undefined) {
-                listeners = [];
-                eventListeners[evtType] = listeners;
-            }
-            if (evtType === "mouseEnter" ||
-                    evtType === "mouseMove" ||
-                    evtType === "mouseLeave") {
-                handlesMouseMove = true;
-            } else if (evtType === "keyDown" ||
-                    evtType === "keyUp") {
-                handlesKeyboard = true;
-            } else if (evtType === "step") {
-                handlesStepping = true;
-            }
-            listeners.push({
-                that: that,
-                callback: listener
-            });
-        };
-		this.forgetListenersFor = function (evtType) {
-			eventListeners[evtType] = undefined;
-		};
-        this.wantsToHandleMouseMove = function () {
-            return handlesMouseMove;
-        };
-        this.wantsToHandleKeyboard = function () {
-            return handlesKeyboard;
-        };
-        this.wantsToHandleStepping = function () {
-            return handlesStepping;
-        };
-    }
-    
-    EventHandler.methods({
-        handleEvent: function (evtType, args) {
-            var listeners = this.listenersFor(evtType);
-            if (listeners.length > 0) {
-                listeners.forEach(function (each) {
-                    each.callback.apply(each.that, args);
-                }, this);
-                return true;
-            }
-            return false;
-        },
-        handleKeyDown: function (evt) {
-            return this.handleEvent("keyDown", [evt]);
-        },
-        handleKeyUp: function (evt) {
-            return this.handleEvent("keyUp", [evt]);
-        },
-        handleMouseDown: function (evt) {
-            return this.handleEvent("mouseDown", [evt]);
-        },
-        handleMouseUp: function (evt) {
-            return this.handleEvent("mouseUp", [evt]);
-        },
-        handleMouseEnter: function (evt, lastCursorPosition) {
-            return this.handleEvent("mouseEnter", [evt, lastCursorPosition]);
-        },
-        handleMouseMove: function (evt, lastCursorPosition) {
-            return this.handleEvent("mouseMove", [evt, lastCursorPosition]);
-        },
-        handleMouseLeave: function (evt, lastCursorPosition) {
-            return this.handleEvent("mouseLeave", [evt, lastCursorPosition]);
-        },
-        handleStep: function (now) {
-            return this.handleEvent("step", [now]);
-        }
-    });
-    
-    return EventHandler;
-})();
+	handleEvent(evtType, args) {
+		var listeners = this.listenersFor(evtType);
+		if (listeners.length > 0) {
+			listeners.forEach(function (each) {
+				each.callback.apply(each.that, args);
+			}, this);
+			return true;
+		}
+		return false;
+	}
+	handleKeyDown(evt) {
+		return this.handleEvent("keyDown", [evt]);
+	}
+	handleKeyUp(evt) {
+		return this.handleEvent("keyUp", [evt]);
+	}
+	handleMouseDown(evt) {
+		return this.handleEvent("mouseDown", [evt]);
+	}
+	handleMouseUp(evt) {
+		return this.handleEvent("mouseUp", [evt]);
+	}
+	handleMouseEnter(evt, lastCursorPosition) {
+		return this.handleEvent("mouseEnter", [evt, lastCursorPosition]);
+	}
+	handleMouseMove(evt, lastCursorPosition) {
+		return this.handleEvent("mouseMove", [evt, lastCursorPosition]);
+	}
+	handleMouseLeave(evt, lastCursorPosition) {
+		return this.handleEvent("mouseLeave", [evt, lastCursorPosition]);
+	}
+	handleStep(now) {
+		return this.handleEvent("step", [now]);
+	}
 
+}
