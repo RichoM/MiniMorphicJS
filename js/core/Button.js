45429c4585801a1617ede1d7eaf84b3f94e4b9b6
Matias Teragni <matias.teragni@gmail.com>
2018-09-18 12:24:48 -0300

    Implemented Button as a class

--- js/core/Button.js
+++ js/core/Button.js
@@ -1,75 +1,73 @@
+class Button extends Morph {
+	constructor() {
+		super();
+		this.color = "#D0062A";
 
-var Button = (function () {
-    
-    Button.inherits(Morph);
-    function Button() {
-        Morph.call(this);
-        
-        this.color("#D0062A");
-        
-        var overColor = "#E40D34";
-        this.overColor = function (val) {
-            if (val !== undefined) {
-                overColor = val;
-            }
-            return overColor;
-        };
-        
-        var pressedColor = "#BB0022";
-        this.pressedColor = function (val) {
-            if (val !== undefined) {
-                pressedColor = val;
-            }
-            return pressedColor;
-        };
-        
-        var textColor = "#000000";
-        this.textColor = function (val) {
-            if (val !== undefined) {
-                textColor = val;
-            }
-            return textColor;
-        };
-        
-        var label = "Press me";
-        this.label = function (val) {
-            if (val !== undefined) {
-                label = val;
-            }
-            return label;
-        };
-        
-        this.extent({ w: 300, h: 100 });
-        this.initializeEventHandling();
-    }
-    
-    Button.methods({
-        initializeEventHandling: function () {
-            var originalColor = this.color();
-            this.on("mouseDown", function () {
-                this.color(this.pressedColor());
-            });
-            this.on("mouseEnter", function () {
-                this.color(this.overColor());
-            });
-            this.on("mouseLeave", function () {
-                this.color(originalColor);
-            });
-            this.on("mouseUp", function () {
-                this.color(originalColor);
-            });
-        },
-        drawOn: function (canvas) {
-            canvas.fillRectangle(this.bounds(), this.color());
-            canvas.drawText(
-                    this.center(), 
-                    this.label(), 
-                    this.textColor(),
-                    "24px Arial",
-                    "center",
-                    "middle");
-        }
-    });
-    
-    return Button;
-})();
+		//TODO: use private fields when it is on the ECMA standard
+		this._overColor = "#E40D34";
+		this._pressedColor = "#BB0022";
+		this._textColor = "#000000";
+		this._label = "Press me";
+
+		this.extent = {
+			w : 300,
+			h : 100
+		};
+		this.initializeEventHandling();
+	}
+	//Properties
+	get overColor() {
+		return this._overColor;
+	}
+	set overColor(val) {
+		this._overColor = val;
+	}
+
+	get pressedColor() {
+		return this._pressedColor;
+	}
+	set pressedColor(val) {
+		this._pressedColor = val;
+	}
+
+	get textColor() {
+		return this._textColor;
+	}
+	set textColor(val) {
+		this._textColor = val;
+	}
+
+	get label() {
+		return this._label;
+	}
+	set label(val) {
+		this._label = val;
+	}
+
+	//Methods
+	initializeEventHandling() {
+		let originalColor = this.color;
+		this.on("mouseDown", function () {
+			this.color = this.pressedColor;
+		});
+		this.on("mouseEnter", function () {
+			this.color = this.overColor;
+		});
+		this.on("mouseLeave", function () {
+			this.color = originalColor;
+		});
+		this.on("mouseUp", function () {
+			this.color = originalColor;
+		});
+	}
+	drawOn(canvas) {
+		canvas.fillRectangle(this.bounds, this.color);
+		canvas.drawText(
+			this.center,
+			this.label,
+			this.textColor,
+			"24px Arial",
+			"center",
+			"middle");
+	}
+}
